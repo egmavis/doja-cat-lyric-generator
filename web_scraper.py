ea919a7554b1fb14bd0dd882edbc6979b763ee2b
Emelia Mavis <emelia.mavis@duke.edu>
2021-12-03 15:05:15 -0500

    update web scraper file

--- web_scraper.py
+++ web_scraper.py
@@ -1,1 +1,25 @@
+"""
+Code for Scraping AZLyrics for Doja Cat Songs
+"""
 
+# import libraries
+import urllib.request as urllib2
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+from unidecode import unidecode
+
+#################################
+# Create Datafame to store Lyrics
+#################################
+
+# specify the url
+webpage = "https://www.azlyrics.com/d/dojacat.html"
+file = "songs.csv"
+songs = pd.read_csv(file)
+
+# query the website and return the html
+page = requests.get(webpage)
+
+# parse the html using beautiful soup
+soup = BeautifulSoup(page.content, "html.parser")
