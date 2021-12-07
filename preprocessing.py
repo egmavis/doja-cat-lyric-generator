a620056763558b9d96ab7d40265398e2a9d90d30
Emelia Mavis <emelia.mavis@duke.edu>
2021-12-06 20:19:34 -0500

    changes

---
+++ preprocessing.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+
+songs = pd.read_csv("doj_songs.csv")
+songs
+
+
+"""
+Tokenize the data
+"""
+
+"""
+Define the alphabet
+"""
+
+"""
+Create training sequences
+"""
+
+"""
+Label encode training sequences
+"""
+
+"""
+One-hot encode the dataset
+"""
