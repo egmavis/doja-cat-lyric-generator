dffd4790ffe140f0dc14b235e2a8078e3042225b
Emelia Mavis <emelia.mavis@duke.edu>
2021-12-12 09:10:21 -0800

    file pushes

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+import re
+import numpy as np
 
 songs = pd.read_csv("doj_songs.csv")
 songs
@@ -9,18 +10,68 @@ songs
 Tokenize the data
 """
 
+# merge all characters together in one string
+text = ""
+clean = ""
+for line in songs["lyrics"]:
+    text = text + str(line).lower()
+    clean = clean + " ".join(re.findall(r"[a-z']+", text))
+
+# find all unique characters
+tokens = re.findall(r"[a-z'\s]", clean)
+
+
 """
 Define the alphabet
 """
+
+characters = sorted(list(set(tokens)))
+len(characters)
+# 28 unique characters
+
+# dictionary for character-to-index mapping
+char_to_index = dict((char, index) for index, char in enumerate(characters))
+
+# dictionary for index-to-character mapping
+index_to_char = dict((index, char) for index, char in enumerate(characters))
+
 
 """
 Create training sequences
 """
 
-"""
-Label encode training sequences
-"""
+# chunk the text into sequences
+maxlen = 20  # n
+step = 1  # length of step at each iteration
+
+# list of sequences
+sequences = []
+
+# list of next characters model should predict
+next_characters = []
+
+# iterate over cleaned text string and each 20-length sequence into list
+for i in range(0, len(clean) - maxlen, step):
+    sequences.append(clean[i : (i + maxlen)])
+    next_characters.append(clean[i + maxlen])
+
 
 """
-One-hot encode the dataset
+Label encode training sequences
+(one-hot encoding)
 """
+
+# create empty matrices for input and output sets
+# input: each n-length sequence in sequences list
+# output: next character after each n-length sequence
+# i.e.: sentence = "hello there"
+#       sequence = "hel"
+#       next char = "l"
+
+x = np.zeros((len(sequences), maxlen, len(characters)), dtype=np.bool)  # input
+y = np.zeros((len(sequences), len(characters)), dtype=np.bool)  # output
+
+for i, chunk in enumerate(sequences):
+    for j, c in enumerate(chunk):
+        x[i, j, char_to_index[c]] = 1
+    y[i, char_to_index[next_characters[i]]] = 1
---
+++ step2model.py
@@ -0,0 +1,102 @@
+"""
+Using Markov Text Generation 
+word-level Lyric Generator
+"""
+
+import pandas as pd
+from statistics import mode
+from collections import defaultdict
+import re
+import random
+import nltk
+
+
+def build_dict(sentence, corpus, n):
+    # sentence_in_progress = sentence
+    index = n - 1
+
+    # print(index)
+    ngram = sentence[-index:]
+    # ngram = sentence_in_progress[-index:]
+
+    # print(ngram)
+
+    dictionary = defaultdict(lambda: 0)
+    for i in range(len(corpus)):
+        current_window = corpus[i : i + index]
+        if current_window == ngram:
+
+            # print(current_window)
+
+            next_word = corpus[i + index]
+            # sentence_in_progress.append(next_word)
+            dictionary[next_word] += 1
+            # ngram = sentence_in_progress[-index:]
+
+            pass
+        pass
+
+    # print(dictionary)
+
+    return dictionary
+
+
+def stupid_backoff(sentence, corpus, n, data, deterministic):
+
+    for i in range(n, 0, -1):
+        if data == {} and i > 1:
+            data = build_dict(sentence, corpus, i)
+            pass
+        elif data == {} and i == 1:
+            if deterministic:
+                data[mode(corpus)] = 1
+                pass
+            else:
+                data[random.choice(corpus)] = 1
+                pass
+            pass
+        pass
+    return data
+
+
+def finish_sentence(sentence, corpus, n, deterministic=False):
+    # punctuation = [".", "!", "?"]
+    while len(sentence) < 200:  # and sentence[-1] not in punctuation:
+        data = build_dict(sentence, corpus, n)
+        if data == {}:
+            data = stupid_backoff(sentence, corpus, n, data, deterministic)
+            pass
+        if deterministic:
+            next_word = max(data, key=lambda x: data[x])
+            sentence.append(next_word)
+            pass
+        else:
+            next_word = random.choice(list(data.keys()))
+            sentence.append(next_word)
+            pass
+    sentence.append(".")
+    return " ".join(sentence)
+
+
+def main():
+    sentence = ["if", "she", "won't"]
+    n = 8
+    songs = pd.read_csv("~/703/doja-cat-lyric-generator/doj_songs.csv")
+
+    # Tokenize Data
+
+    # merge all characters into one string
+    clean = ""
+    for line in songs["lyrics"]:
+        clean = clean + " ".join(re.findall(r"[a-z']+", line))
+
+    corpus = nltk.word_tokenize(clean)
+    # print(corpus)
+
+    flag = True
+
+    print(finish_sentence(sentence, corpus, n, flag))
+
+
+if __name__ == "__main__":
+    main()
